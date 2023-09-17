5da1db4774680a31f3cb371fca16b4cb20a976de
uimkatali <b8bf6b5@gmail.com>
2023-09-17 21:20:29 +0300

    Add project files.

---
+++ lab1ex5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace lab1ex1
+{
+    /*
+     Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double[] strings = { a, b };
+            Array.Sort(strings);    
+            foreach (double item in strings) Console.WriteLine(item);
+        }
+    }
+}
